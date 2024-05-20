9b056941089fda134fe274d57d10066b1c3768b7
SchZolee80 <sch.zolee80@gmail.com>
2024-05-20 07:46:54 +0200

    start.spring.io

---
+++ src/main/java/com/example/runrace/RunRaceApplication.java
@@ -0,0 +1,13 @@
+package com.example.runrace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RunRaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RunRaceApplication.class, args);
+	}
+
+}
